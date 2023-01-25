466dca26ecc3304cce202e0ef8fd10a5a911e5ce
easycode256 <easycode256@gmail.com>
2023-01-25 11:31:22 +0100

    add exos and solutions for variables

---
+++ exos.py
@@ -0,0 +1,15 @@
+# donner la valeur d'initialisation des variables
+
+a = 10
+b = 20 * 3
+c = a + 5
+d =  b * 2
+e = 5 / 2
+f = 5 // 2
+
+
+# Donner la valeur finale des variables a et b
+a = 10
+b = a * 2
+a = b + a
+b = a - 10 
